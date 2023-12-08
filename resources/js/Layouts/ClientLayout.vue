18f2a284150fd4a94f407aa7bf7aed1e16ca3508
Minhazul-Islam18 <minhaz019166@gmail.com>
2023-12-08 22:58:33 +0600

    Bug fixed

--- resources/js/Layouts/ClientLayout.vue
+++ resources/js/Layouts/ClientLayout.vue
@@ -5,7 +5,7 @@ console.log('ok');
     <header class=" pt-4 pb-3 border-b">
         <h1 class=" text-center">Layout</h1>
     </header>
-    <div class="container mx-auto bg-[#f2f2f2]">
+    <div class="container mx-auto">
         <slot />
     </div>
     <footer class="py-6 border-t">
