8eb4313dfada84eb53d0abe4f25f8cc5ccc3537f
Minhazul-Islam18 <minhaz019166@gmail.com>
2023-12-09 12:33:24 +0600

    Today's time widget design

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -6,72 +6,57 @@ import ClientLayout from '@/Layouts/ClientLayout.vue';
     <ClientLayout>
         <div class="py-12">
             <div class=" bg-white rounded-lg px-7 py-4" id="prayertimes-card">
-                <div class="pt-card-header">
+                <div class="pt-card-header flex flex-col sm:flex-row flex-wrap justify-between">
                     <div class="d-flex flex-column">
-                        <h1 class="boxCard-title mb-1">
+                        <h1 class="boxCard-title mb-1 font-extrabold text-3xl pb-1 border-b">
                             Prayer Times in&nbsp;Dhaka</h1>
-                        <a href="/world/" class="font-primary font-sm change-loc gtm-pt-chloc">Wrong Location?</a>
+                        <a href="/world/" class="font-primary font-sm change-loc gtm-pt-chloc text-emerald-500">Wrong
+                            Location?</a>
                     </div>
                     <div class="pt-date font-dark font-sm">
                         <p>8&nbsp;December,&nbsp;2023</p>
                         <p class="font-weight-bold pt-date-right">23&nbsp;Jumada Al-Awwal,&nbsp;1445</p>
                     </div>
                 </div>
-                <div class="pt-card-tiles d-flex slick-initialized slick-slider" dir="">
-                    <div class="slick-list draggable">
-                        <div class="slick-track"
-                            style="opacity: 1; width: 30000px; transform: translate3d(-10px, 0px, 0px);">
-                            <div class="prayerTiles fajar-tile pt-active fajar slick-slide slick-current slick-active"
-                                data-slick-index="0" aria-hidden="false" tabindex="0">
-                                <p>
-                                    <span class="prayername ">Upcoming Prayer<br>Fajr</span><span class="pt-timer"
-                                        id="clockdiv">08:21:36</span>
-                                    <span class="prayertime">05:09 AM</span><span class="pt-timer" id="clockdiv"></span>
-                                </p><a class="btn-refresh" href="./" style="opacity: 0;" tabindex="0"><img alt="refresh"
-                                        src="/static/live180/themes/theme20/images/refresh-btn.png"></a>
-                            </div>
-                            <div class="prayerTiles sunrise-tile slick-slide slick-active" data-slick-index="1"
-                                aria-hidden="false" tabindex="0">
-                                <p>
-                                    <span class="prayername">Sunrise </span>
-                                    <span class="prayertime">06:28 AM</span>
-                                </p>
-                            </div>
-                            <div class="prayerTiles dhuhar-tile slick-slide slick-active" data-slick-index="2"
-                                aria-hidden="false" tabindex="0">
-                                <p>
-                                    <span class="prayername">Dhuhr</span>
-                                    <span class="prayertime">11:51 AM</span>
-                                </p>
-                            </div>
-                            <div class="prayerTiles asr-tile slick-slide slick-active" data-slick-index="3"
-                                aria-hidden="false" tabindex="0">
-                                <p>
-                                    <span class="prayername">Asr</span>
-                                    <span class="prayertime">02:52 PM</span>
-                                </p>
-                            </div>
-                            <div class="prayerTiles maghrib-tile slick-slide slick-active" data-slick-index="4"
-                                aria-hidden="false" tabindex="0">
-                                <p>
-                                    <span class="prayername">Maghrib</span>
-                                    <span class="prayertime">05:12 PM</span>
-                                </p>
-                            </div>
-                            <div class="prayerTiles isha-tile slick-slide slick-active" data-slick-index="5"
-                                aria-hidden="false" tabindex="0">
-                                <p>
-                                    <span class="prayername">Isha</span>
-                                    <span class="prayertime">06:32 PM</span>
-                                </p>
-                            </div>
-                        </div>
+                <div id="TimesToday" class=" flex flex-col sm:flex-row flex-wrap gap-[1%] items-end justify-between">
+                    <div
+                        class="rounded-md px-12 py-8 bg-[url('https://www.islamicfinder.org/static/live180/themes/theme20/images/fajr.png')] bg-cover bg-no-repeat bg-center w-[20%] text-white">
+                        <p>
+                            <span class="prayername text-center text-md block">Upcoming Prayer<br>Fajr</span>
+                            <span class=" font-extrabold text-4xl block" id="clockdiv">08:21:36</span>
+                            <span class="text-center block">05:09 AM</span>
+                        </p>
                     </div>
-
-
-
-
-
+                    <div class=" rounded-md px-4 py-8 bg-gray-200 w-[15%]">
+                        <p>
+                            <span class="text-md text-center block">Sunrise </span>
+                            <span class="text-center text-xl block">06:28 AM</span>
+                        </p>
+                    </div>
+                    <div class=" rounded-md px-4 py-8 bg-gray-200 w-[15%]">
+                        <p>
+                            <span class="text-md text-center block">Sunrise </span>
+                            <span class="text-center text-xl block">06:28 AM</span>
+                        </p>
+                    </div>
+                    <div class=" rounded-md px-4 py-8 bg-gray-200 w-[15%]">
+                        <p>
+                            <span class="text-md text-center block">Sunrise </span>
+                            <span class="text-center text-xl block">06:28 AM</span>
+                        </p>
+                    </div>
+                    <div class=" rounded-md px-4 py-8 bg-gray-200 w-[15%]">
+                        <p>
+                            <span class="text-md text-center block">Sunrise </span>
+                            <span class="text-center text-xl block">06:28 AM</span>
+                        </p>
+                    </div>
+                    <div class=" rounded-md px-4 py-8 bg-gray-200 w-[15%]">
+                        <p>
+                            <span class="text-md text-center block">Sunrise </span>
+                            <span class="text-center text-xl block">06:28 AM</span>
+                        </p>
+                    </div>
                 </div>
                 <!-- <script type="text/template" id="timer-template"><span class="pt-timer" id="clockdiv"></span></script>
                 <script type="text/template" id="btn-refresh-template">
